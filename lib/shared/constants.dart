ce923f5adf131bfe2bf659fecd4c02ea8bd89583
Pun <impan_1611_2544@hotmail.com>
2024-03-20 22:45:34 +0700

    update:login ui page

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/material.dart';
+
 class Constants {
   static String appId = "1:402928775255:web:0e0ea5c48f8b72e0f43312";
   static String apikey = "AIzaSyBbSzi7cP1svTYJRtxTr6Ob00yhze70jlY";
   static String messagingSenderId = "402928775255";
   static String projectId = "chatappflutter-426c5";
+  final primaryColor = const Color(0xFFee7b64);
 }
