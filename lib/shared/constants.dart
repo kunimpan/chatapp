d5f2b02559c1c5dd4079b5bbd02dedbbaa59a17c
Pun <impan_1611_2544@hotmail.com>
2024-03-20 21:25:01 +0700

    create:login and home

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:402928775255:web:0e0ea5c48f8b72e0f43312";
+  static String apikey = "AIzaSyBbSzi7cP1svTYJRtxTr6Ob00yhze70jlY";
+  static String messagingSenderId = "402928775255";
+  static String projectId = "chatappflutter-426c5";
+}
