9e148fa57957226ce6447277c37f1d4f68a111a1
pun <impan_1611_2544@Hotmail.com>
2024-03-21 22:53:54 +0700

    now can login

--- lib/service/auth_service.dart
+++ lib/service/auth_service.dart
@@ -1,3 +1,4 @@
+import 'package:chatapp/helper/helper_function.dart';
 import 'package:chatapp/service/database_server.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -5,6 +6,19 @@ class AuthService {
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 
   // login
+  Future loginWithUserNameAndPassword(String email, String password) async {
+    try {
+      User user = (await firebaseAuth.signInWithEmailAndPassword(
+              email: email, password: password))
+          .user!;
+
+      if (user != null) {
+        return true;
+      }
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
 
   // register
   Future registerUserWithEmailAndPassword(
@@ -16,7 +30,7 @@ class AuthService {
 
       if (user != null) {
         // call out database service to update the user data.
-        await DatabaseService(uid: user.uid).updateUserData(fullName, email);
+        await DatabaseService(uid: user.uid).savingUserData(fullName, email);
         return true;
       }
     } on FirebaseAuthException catch (e) {
@@ -25,4 +39,14 @@ class AuthService {
   }
 
   // signout
+  Future signOut() async {
+    try {
+      await HelperFunctions.saveUserLoggedInStatus(false);
+      await HelperFunctions.saveUserEmailSF("");
+      await HelperFunctions.saveUserNameSF("");
+      await firebaseAuth.signOut();
+    } catch (e) {
+      return null;
+    }
+  }
 }
